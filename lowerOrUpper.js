8d6410c56c3a4526a3457154d029881779db3bc5
Elia Kuzmanova <eliamitova7@gmail.com>
2023-01-29 21:57:31 +0100

    add homework

---
+++ lowerOrUpper.js
@@ -0,0 +1,10 @@
+function lowerOrUpper(a) {
+    if (a == a.toUpperCase()) {
+        console.log("upper-case");
+    } else {
+        console.log("lower-case");
+    }
+
+}
+lowerOrUpper('L');
+lowerOrUpper('f');
